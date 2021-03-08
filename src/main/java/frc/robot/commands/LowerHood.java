b304729044b7c5671ef93c0775085f4a69c49019
HFeeney <haydenbfeeney@gmail.com>
2021-03-08 12:27:52 -0800

    Lower hood sets hood motor to zero upon end

--- src/main/java/frc/robot/commands/LowerHood.java
+++ src/main/java/frc/robot/commands/LowerHood.java
@@ -28,7 +28,9 @@ public class LowerHood extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    hood.angleHood(0);
+  }
 
   // Returns true when the command should end.
   @Override
